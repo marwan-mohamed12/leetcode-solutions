3392bf01842c73e28dc70c188652a89ee466a7ec
Marawan Mohamed <40841193+marwan-mohamed12@users.noreply.github.com>
2024-05-31 18:29:52 +0300

    Delete Database/1978-Employees-Whose-Manager-Left-the-Company.sql

--- Database/1978-Employees-Whose-Manager-Left-the-Company.sql
+++
@@ -1,6 +0,0 @@
-# Write your MySQL query statement below
-SELECT employee_id 
-from Employees 
-WHERE salary < 30000 
-AND manager_id NOT IN (select employee_id from Employees)
-ORDER BY employee_id;

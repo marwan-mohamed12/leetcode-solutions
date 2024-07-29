7478512f7f548ca437c411670d556227fe9aafd6
Marawan Mohamed <40841193+marwan-mohamed12@users.noreply.github.com>
2024-07-29 20:53:40 +0300

    [217] [Time Beats: 89.20%] [Memory Beats: 59.12%] - LeetPush

---
+++ Hash Tables/217-Contains-Duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> numSet = new HashSet<>();
+
+        for (int num: nums) {
+            if(numSet.contains(num)) {
+                return true;
+            }
+
+            numSet.add(num);
+        }
+
+        return false;
+    }
+}
